612f070b7258f4ca58d2de726102df4abad51ee4
pjecty <dldydgus6302@naver.com>
2023-07-03 17:14:16 +0900

    + marketSave 끝 + marketList 끝 + marketDetail  ( 좋아요 -> 구매하기 -> 채팅 ) 순서기능진행 + marketreport  porm 작성

--- src/main/java/com/example/weartheweather/repository/MarketProductRepository.java
+++ src/main/java/com/example/weartheweather/repository/MarketProductRepository.java
@@ -3,6 +3,8 @@ package com.example.weartheweather.repository;
 import com.example.weartheweather.entity.MarketProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MarketProductRepository extends JpaRepository<MarketProductEntity, Long> {
 
 }
