1d5132c01b4af5e6be5aff8ff7a7030b05b24f1e
gkdudquf <gkdudquf@gmail.com>
2023-07-13 13:38:20 +0900

    memberDetail △ 알림기능..ing memberList △ 페이징처리 △ 검색기능 memberUpdate △ 이미지파일수정 memberSave △ 게시글 에디터 추가

--- src/main/java/com/example/weartheweather/repository/AlarmRepository.java
+++ src/main/java/com/example/weartheweather/repository/AlarmRepository.java
@@ -1,7 +1,12 @@
 package com.example.weartheweather.repository;
 
 import com.example.weartheweather.entity.AlarmEntity;
+import com.example.weartheweather.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlarmRepository  extends JpaRepository<AlarmEntity,Long> {
+
+    List<AlarmEntity> findByWriterMemberEntity(MemberEntity loginMemberEntity);
 }
