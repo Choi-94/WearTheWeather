07f1c9c2c35b680fe0b4623ab6ecc0b6a2fcb2b3
gkdudquf <gkdudquf@gmail.com>
2023-07-13 17:04:35 +0900

    memberDetail △ 알림출력중 memberList △ 페이징처리 △ 검색기능 memberUpdate △ 이미지파일수정 memberSave △ 게시글 에디터 추가

--- src/main/java/com/example/weartheweather/controller/AlarmController.java
+++ src/main/java/com/example/weartheweather/controller/AlarmController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -32,7 +33,6 @@ public class AlarmController {
             List<AlarmDTO> alarmDTOList = alarmService.findByMyAlarm(memberNickName);
             return new ResponseEntity<>(alarmDTOList, HttpStatus.OK);
         }
-
 
 
 }
