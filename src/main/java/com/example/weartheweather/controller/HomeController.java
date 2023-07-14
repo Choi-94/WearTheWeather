535fcf28d28561d9fce4cb7206e8cf438c3a2a65
pjecty <dldydgus6302@naver.com>
2023-07-14 20:33:33 +0900

    index 수정 ( 주간코디 작업중 )

--- src/main/java/com/example/weartheweather/controller/HomeController.java
+++ src/main/java/com/example/weartheweather/controller/HomeController.java
@@ -1,18 +1,43 @@
 package com.example.weartheweather.controller;
 
+import com.example.weartheweather.dto.AdminBoardDTO;
+import com.example.weartheweather.dto.MarketLikesDTO;
+import com.example.weartheweather.dto.MarketProductDTO;
+import com.example.weartheweather.dto.MemberBoardDTO;
+import com.example.weartheweather.service.AdminBoardService;
+import com.example.weartheweather.service.MarketProductService;
+import com.example.weartheweather.service.MemberBoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+    private final MarketProductService marketProductService;
+    private final MemberBoardService memberBoardService;
+    private final AdminBoardService adminBoardService;
+
     @GetMapping("/")
-    public String index(){
+    public String findByDetailList(Model model, @PageableDefault(size = 15) Pageable pageable, @RequestParam(value = "type", required = false, defaultValue = "") String type,
+                                   @RequestParam(value = "q", required = false, defaultValue = "") String q) {
+
+        List<AdminBoardDTO> adminBoardDTOList = adminBoardService.findAll();
+        model.addAttribute("adminBoardDTOList", adminBoardDTOList);
+
+        Page<MemberBoardDTO> memberBoardDTOList = memberBoardService.findAll(pageable,type,q);
+        model.addAttribute("memberBoardList", memberBoardDTOList);
+
+        List<MarketProductDTO> marketProductDTOList = marketProductService.findAll();
+        model.addAttribute("marketProductList", marketProductDTOList);
         return "index";
     }
-    @GetMapping("/access-denied")
-    public String accessDenied(){
-        return "accessDenied";
-    }
-
-
 }
