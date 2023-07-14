3b16f8047767ab9837773851b43d4edf1933945c
gkdudquf <gkdudquf@gmail.com>
2023-07-14 15:12:55 +0900

    memberDetail + 알림 미확인내역만 출력

--- src/main/java/com/example/weartheweather/repository/AlarmRepository.java
+++ src/main/java/com/example/weartheweather/repository/AlarmRepository.java
@@ -5,14 +5,16 @@ import com.example.weartheweather.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AlarmRepository  extends JpaRepository<AlarmEntity,Long> {
 
-    List<AlarmEntity> findByWriterMemberEntity(MemberEntity loginMemberEntity);
+    @Query("select a from AlarmEntity a where a.isReadFlag = 1 and a.writerMemberEntity = :loginMemberEntity")
+    List<AlarmEntity> findByWriterMemberEntity(@Param("loginMemberEntity") MemberEntity loginMemberEntity);
 
     @Modifying
     @Query(value = "update AlarmEntity a set a.isReadFlag = 0 where a.id=:id")
-    void updateIsReadFlag(Long id);
+    void updateIsReadFlag(@Param("id") Long id);
 }
