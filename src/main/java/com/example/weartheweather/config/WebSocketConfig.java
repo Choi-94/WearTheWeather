2242787d72e46150ebd2e3cbbfbcf9e6c887aba0
Choi-94 <qaz6168@naver.com>
2023-07-18 15:09:18 +0900

    도영 - 0718 앱소캣을 이용한 관리자와 다수 사용자가 채팅할 수 있는 채팅방을 구현

--- src/main/java/com/example/weartheweather/config/WebSocketConfig.java
+++ src/main/java/com/example/weartheweather/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "http://localhost:8045/chat").setAllowedOrigins("*")
+        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*")
                 .addInterceptors(new HttpSessionHandshakeInterceptor());   // interceptor for adding httpsession into websocket session
     }
 }
